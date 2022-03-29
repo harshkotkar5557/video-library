66a50abd9fb5acd1fa0dd80edd9c560643b79059
Haresh Kotkar <75067419+harshkotkar5557@users.noreply.github.com>
2022-03-29 17:04:33 +0530

    install-react and add navbar

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './components/navbar';
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Navbar/>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/navbar/index.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <nav class="navbar_std">
+          <header class="d-flex justify-space-bw align-center">
+            <div class="brandName"><span className='grey-color'>U</span>Tube</div>
+            <div className='nav-right-box'>
+              <button class="btn default">Login</button>
+            </div>
+             
+        </header>
+  </nav>
+  )
+}
+
+export default Navbar
